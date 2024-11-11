e525a7770df1d5575bc7a4f63e8b7fd2527e1245
Ida <77254066+idaocracy@users.noreply.github.com>
2024-11-11 16:48:21 +0900

    Reverted 1.20 workaround for 1.19 branch

--- Runtime/ObjectData/Values/VariableData.cs
+++ Runtime/ObjectData/Values/VariableData.cs
@@ -1,58 +1,41 @@
 ﻿using Naninovel;
-using Naninovel.Commands;
+using NaninovelSceneAssistant;
 using System;
 
-namespace NaninovelSceneAssistant
+public class VariableData : IDisposable
 {
-    public class VariableData : IDisposable
+    public string Name { get; }
+    public string Value { get => getValue(); set { if(SceneAssistantManager.IsAvailable) setValue(value); } }
+    public bool Changed { get; set; }
+    //public string State { get; set; }
+
+    private readonly Func<string> getValue;
+    private readonly Action<string> setValue;
+
+    protected SceneAssistantManager SceneAssistantManager => Engine.GetService<SceneAssistantManager>();
+
+    public VariableData(string name)
     {
-        public string Name { get; }
-        public string Value { get => getValue(); set { if (SceneAssistantManager.IsAvailable) setValue(value); } }
-        public bool Changed { get; set; }
-        //public string State { get; set; }
+        Name = name;
+        
+        var customVariableManager = Engine.GetService<ICustomVariableManager>();
+        getValue = () => customVariableManager.GetVariableValue(Name);
+        setValue = (value) => customVariableManager.SetVariableValue(Name, value);
 
-        private readonly Func<string> getValue;
-        private readonly Action<string> setValue;
+        //State = Value;
 
-        protected SceneAssistantManager SceneAssistantManager => Engine.GetService<SceneAssistantManager>();
+        SceneAssistantManager.OnSceneAssistantReset += HandleReset;
+    }
 
-        public VariableData(string name)
-        {
-            Name = name;
-
-            var customVariableManager = Engine.GetService<ICustomVariableManager>();
-
-            // 1.20 workaround
-            getValue = () => Naninovel.ExpressionEvaluator.Evaluate<string>(Name);
-            setValue = (value) => new SetCustomVariable { Expression = Name + "=" + (IsStringValue(value) ? "\"" + value + "\"" : value) }.ExecuteAsync().Forget();
-
-            // 1.19 legacy
-            //getValue = () => customVariableManager.GetVariableValue(Name);
-            //setValue = (value) => customVariableManager.SetVariableValue(Name, value);
-
-            //State = Value;
-
-            SceneAssistantManager.OnSceneAssistantReset += HandleReset;
-
-            bool IsStringValue(string value)
-            {
-                if (float.TryParse(value, out _)) return false;
-                else if (int.TryParse(value, out _)) return false;
-                else if (bool.TryParse(value, out _)) return false;
-                else return true;
-            }
-        }
-
-        private void HandleReset()
-        {
-            //todo get the previous state, not initial value
-            Changed = false;
-        }
-
-        public void DisplayField(ICustomVariableLayout layout) => layout.VariableField(this);
-        public void Dispose()
-        {
-            SceneAssistantManager.OnSceneAssistantReset -= HandleReset;
-        }
+    private void HandleReset()
+    {
+        //todo get the previous state, not initial value
+        Changed = false;
+    }
+    
+    public void DisplayField(ICustomVariableLayout layout) => layout.VariableField(this);
+    public void Dispose()
+    {
+        SceneAssistantManager.OnSceneAssistantReset -= HandleReset;
     }
 }
